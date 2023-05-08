3ff2e08a2f6f813435456c2ceed6e62cfc921d0a
Safin Shihab <shihab3750@gmail.com>
2023-05-08 01:40:45 -0400

    finished hw8

--- public_html/interest/js/custom.js
+++ public_html/interest/js/custom.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-  $(".got-it-btn").click(function () {
-    $("#add-to-cart-box").css("display", "none");
-  });
   $(".cartt").click(function () {
     $("#add-to-cart-box").css("display", "flex");
+    $("#atcb-content").show();
+    $("#atcb-content").append(
+      "<div id='atcb-text'>You have successfully added this item to your cart!</div>"
+    );
+  });
+
+  $(".got-it-btn").click(function () {
+    $("#add-to-cart-box").css("display", "none");
+    $("#atcb-content").hide();
+    $("#atcb-content #atcb-text").remove();
   });
 });
